caec5a85ab14cbc2eb7adb545b7979f952569637
martinwoo7 <martinwooo@hotmail.com>
2024-05-19 16:30:33 -0700

    edited contact page

--- app/(personal)/contact/page.tsx
+++ app/(personal)/contact/page.tsx
@@ -1,43 +1,191 @@
-import { Metadata } from 'next';
-import Image from 'next/image';
-import DefaultForm from '@/app/ui/about/Form';
+import { Metadata } from "next";
+import Image from "next/image";
+import DefaultForm from "@/app/ui/about/Form";
+import Link from "next/link";
 
 export const metadata: Metadata = {
-    title: 'Contact Us',
+	title: "Contact Us",
 };
 export default function Page() {
-    return (
-        <main className="m-0">
-            <section className="">
-                <div className="relative flex mx-auto w-full h-[45vh]">
-                    <Image
-                        src="/kitchen.jpg"
-                        alt="about us banner"
-                        fill
-                        quality={50}
-                        className="object-cover object-center"
-                    />
+	return (
+		<main className="mx-auto max-w-7xl px-6 lg:px-8 mt-24 sm:mt-32 lg:mt-40">
+			<section className="mx-auto max-w-2xl lg:max-w-none">
+				<h1>
+					<span className="block font-display text-base font-semibold">
+						Contact us
+					</span>
+					<span className="sr-only">-</span>
+					<span className="mt-6 block max-w-5xl font-display text-5xl font-medium tracking-tight [text-wrap:balance] sm:text-6xl">
+						Let&apos;s work together
+					</span>
+				</h1>
+				<div className="mt-6 max-w-3xl text-xl">
+					We can&apos;t wait to hear from you.
+				</div>
+			</section>
 
-                    <div className="absolute inset-0 bg-[#111111]/30 " />
+			<section className="mx-auto max-w-2xl lg:max-w-none mt-24 sm:mt-32 lg:mt-40">
+				<div className="grid grid-cols-1 gap-x-8 gap-y-24 lg:grid-cols-2">
+					<div className="lg:order-last">
+						<form action="">
+							<h2 className="font-display text-base font-semibold">
+								Work inquiries
+							</h2>
+							<div className="isolate mt-6 -space-y-px rounded-2xl ">
+								<div className="group relative z-0 transition-all focus-within:z-10">
+									<input
+										type="text"
+										name="name"
+										autoComplete="name"
+										placeholder=" "
+										id=":S1:"
+										className="peer block w-full border border-neutral-300 bg-transparent px-6 pb-4 pt-12 text-base/6 text-neutral-950 ring-4 ring-transparent transition focus:border-neutral-950 focus:outline-none focus:ring-neutral-950/5 group-first:rounded-t-2xl group-last:rounded-b-2xl"
+									/>
+									<label
+										htmlFor=":S1:"
+										className="pointer-events-none absolute left-6 top-1/2 -mt-3 origin-left text-base/6 text-neutral-500 transition-all duration-200 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:font-semibold peer-focus:text-neutral-950 peer-[:not(:placeholder-shown)]:-translate-y-4 peer-[:not(:placeholder-shown)]:scale-75 peer-[:not(:placeholder-shown)]:font-semibold peer-[:not(:placeholder-shown)]:text-neutral-950"
+									>
+										Name
+									</label>
+								</div>
+								<div className="group relative z-0 transition-all focus-within:z-10">
+									<input
+										type="email"
+										name="email"
+										autoComplete="email"
+										placeholder=" "
+										id=":S2:"
+										className="peer block w-full border border-neutral-300 bg-transparent px-6 pb-4 pt-12 text-base/6 text-neutral-950 ring-4 ring-transparent transition focus:border-neutral-950 focus:outline-none focus:ring-neutral-950/5 group-first:rounded-t-2xl group-last:rounded-b-2xl"
+									/>
+									<label
+										htmlFor=":S2:"
+										className="pointer-events-none absolute left-6 top-1/2 -mt-3 origin-left text-base/6 text-neutral-500 transition-all duration-200 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:font-semibold peer-focus:text-neutral-950 peer-[:not(:placeholder-shown)]:-translate-y-4 peer-[:not(:placeholder-shown)]:scale-75 peer-[:not(:placeholder-shown)]:font-semibold peer-[:not(:placeholder-shown)]:text-neutral-950"
+									>
+										Email
+									</label>
+								</div>
+								<div className="group relative z-0 transition-all focus-within:z-10">
+									<input
+										type="text"
+										name="company"
+										autoComplete="organization"
+										placeholder=" "
+										id=":S3"
+										className="peer block w-full border border-neutral-300 bg-transparent px-6 pb-4 pt-12 text-base/6 text-neutral-950 ring-4 ring-transparent transition focus:border-neutral-950 focus:outline-none focus:ring-neutral-950/5 group-first:rounded-t-2xl group-last:rounded-b-2xl"
+									/>
+									<label
+										htmlFor=":S3:"
+										className="pointer-events-none absolute left-6 top-1/2 -mt-3 origin-left text-base/6 text-neutral-500 transition-all duration-200 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:font-semibold peer-focus:text-neutral-950 peer-[:not(:placeholder-shown)]:-translate-y-4 peer-[:not(:placeholder-shown)]:scale-75 peer-[:not(:placeholder-shown)]:font-semibold peer-[:not(:placeholder-shown)]:text-neutral-950"
+									>
+										Company
+									</label>
+								</div>
+								<div className="group relative z-0 transition-all focus-within:z-10">
+									<input
+										type="tel"
+										name="phone"
+										autoComplete="tel"
+										placeholder=" "
+										id=":S4:"
+										className="peer block w-full border border-neutral-300 bg-transparent px-6 pb-4 pt-12 text-base/6 text-neutral-950 ring-4 ring-transparent transition focus:border-neutral-950 focus:outline-none focus:ring-neutral-950/5 group-first:rounded-t-2xl group-last:rounded-b-2xl"
+									/>
+									<label
+										htmlFor=":S4:"
+										className="pointer-events-none absolute left-6 top-1/2 -mt-3 origin-left text-base/6 text-neutral-500 transition-all duration-200 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:font-semibold peer-focus:text-neutral-950 peer-[:not(:placeholder-shown)]:-translate-y-4 peer-[:not(:placeholder-shown)]:scale-75 peer-[:not(:placeholder-shown)]:font-semibold peer-[:not(:placeholder-shown)]:text-neutral-950"
+									>
+										Phone
+									</label>
+								</div>
+								<div className="group relative z-0 transition-all focus-within:z-10">
+									<input
+										type="text"
+										name="message"
+										placeholder=" "
+										id=":S5:"
+										className="peer block w-full border border-neutral-300 bg-transparent px-6 pb-4 pt-12 text-base/6 text-neutral-950 ring-4 ring-transparent transition focus:border-neutral-950 focus:outline-none focus:ring-neutral-950/5 group-first:rounded-t-2xl group-last:rounded-b-2xl"
+									/>
+									<label
+										htmlFor=":S5:"
+										className="pointer-events-none absolute left-6 top-1/2 -mt-3 origin-left text-base/6 text-neutral-500 transition-all duration-200 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:font-semibold peer-focus:text-neutral-950 peer-[:not(:placeholder-shown)]:-translate-y-4 peer-[:not(:placeholder-shown)]:scale-75 peer-[:not(:placeholder-shown)]:font-semibold peer-[:not(:placeholder-shown)]:text-neutral-950"
+									>
+										Message
+									</label>
+								</div>
+							</div>
+							<button className="mt-10 inline-flex rounded-full px-4 py-1.5 text-sm font-semibold transition bg-white text-black">
+								let&apos;s work together
+							</button>
+						</form>
+					</div>
 
-                    <div className="absolute inset-x-0 inset-y-auto self-center text-center">
-                        <div className="text-white tracking-wider px-5">
-                            <h1 className="text-6xl mb-6 text-thin">
-                                Contact Us
-                            </h1>
-                            <p className="text-center">
-                                We would love to hear from you!
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </section>
+					<div>
+						<h2 className="font-display text-base font-semibold">
+							Our offices
+						</h2>
+						<p className="mt-6 text-base text-gray-300">
+							Prefer doing things in person? We don&apos;t but we have
+							to list our addresses here for legal reasons.
+						</p>
+						<ul
+							role="list"
+							className="mt-10 grid grid-cols-1 gap-8 sm:grid-cols-2"
+						>
+							<li>
+								<address className="text-sm not-italic text-gray-300">
+									<strong className="text-white">
+										Copenhagen
+									</strong>
+									<br />
+									1 Carlsberg Gate
+									<br />
+									1260, København, Denmark
+								</address>
+							</li>
+							<li>
+								<address className="text-sm not-italic text-gray-300">
+									<strong className="text-white">
+										Billund
+									</strong>
+									<br />
+									24 Lego Allé
+									<br />
+									7190, Billund, Denmark
+								</address>
+							</li>
+						</ul>
 
-            <section className="content-wrapper flex flex-col flex-wrap items-center max-w-7xl mx-auto">
-                <div className="contact-form mx-12 my-12">
-                    <DefaultForm />
-                </div>
-            </section>
-        </main>
-    );
+						<div className="mt-16 pt-16 relative before:absolute after:absolute before:bg-white after:bg-white/20 before:left-0 before:top-0 before:h-px before:w-6 after:left-8 after:right-0 after:top-0 after:h-px">
+							<h2 className="font-display text-base font-semibold">
+								Email us
+							</h2>
+							<dl className="mt-6 grid grid-cols-1 gap-8 text-sm sm:grid-cols-2">
+								<div>
+									<dt className="font-semibold">Careers</dt>
+									<dd>
+										<Link href="/#">
+											careers@studioagency.com
+										</Link>
+									</dd>
+								</div>
+								<div>
+									<dt className="font-semibold">Press</dt>
+									<dd>
+										<Link href="/#">
+											press@studioagency.com
+										</Link>
+									</dd>
+								</div>
+							</dl>
+						</div>
+						{/* <div className="mt-16 pt-16 relative before:absolute after:absolute before:bg-white after:bg-white/20 before:left-0 before:top-0 before:h-px before:w-6 after:left-8 after:right-0 after:top-0 after:h-px">
+                            <h2>Follow us</h2>
+                            <ul role="list" className="flex gap-x-10 mt-6">
+                                <li></li>
+                            </ul>
+                        </div> */}
+					</div>
+				</div>
+			</section>
+		</main>
+	);
 }
